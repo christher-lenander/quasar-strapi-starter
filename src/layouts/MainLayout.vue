b5ddde2916f77c3e57dc8f07dfe565a5f13eab62
Christher Lenander <christher.lenander@enklare.se>
2022-11-28 14:41:02 +0100

    Updated Main Layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="bg-third">
+    <q-header class="bg-custom-dark-blue">
       <q-toolbar>
         <q-btn
           flat
@@ -17,7 +17,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
       <q-list>
         <q-item-label header> Essential Links </q-item-label>
 
